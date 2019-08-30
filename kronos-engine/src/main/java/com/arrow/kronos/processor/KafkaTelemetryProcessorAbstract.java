afc5ab85d1955da4e0e2bd2d4eeefbf1c682df73
Tam Nguyen <ntam4713@gmail.com>
2019-08-30 17:26:32 -0500

    check and create kafka topic during startup

--- kronos-engine/src/main/java/com/arrow/kronos/processor/KafkaTelemetryProcessorAbstract.java
+++ kronos-engine/src/main/java/com/arrow/kronos/processor/KafkaTelemetryProcessorAbstract.java
@@ -1,10 +1,15 @@
 package com.arrow.kronos.processor;
 
+import java.util.Collections;
+import java.util.Set;
 import java.util.regex.Pattern;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaAdmin;
 
 import com.arrow.kronos.KronosEngineConstants;
 import com.arrow.kronos.KronosEngineContext;
@@ -15,59 +20,81 @@ import moonstone.acs.JsonUtils;
 
 public abstract class KafkaTelemetryProcessorAbstract extends KafkaConsumerAbstract implements CommandLineRunner {
 
-    @Autowired
-    private KronosEngineContext context;
-    @Autowired
-    private Environment env;
+	@Autowired
+	private KronosEngineContext context;
+	@Autowired
+	private Environment env;
+	@Autowired
+	private KafkaAdmin admin;
 
-    private final String suffix;
+	private final String suffix;
 
-    public KafkaTelemetryProcessorAbstract(String suffix) {
-        this.suffix = suffix;
-    }
+	public KafkaTelemetryProcessorAbstract(String suffix) {
+		this.suffix = suffix;
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        String method = "run";
-        String enabled = getEnvProperty("enabled", "true");
-        if (enabled.equalsIgnoreCase("true")) {
-            start();
-        } else {
-            logWarn(method, "%s is DISABLED!", getClass().getSimpleName());
-        }
-    }
+	@Override
+	public void run(String... args) throws Exception {
+		String method = "run";
+		String enabled = getEnvProperty("enabled", "true");
+		if (enabled.equalsIgnoreCase("true")) {
+			start();
+		} else {
+			logWarn(method, "%s is DISABLED!", getClass().getSimpleName());
+		}
+	}
 
-    @Override
-    protected Pattern getTopicPattern() {
-        String method = "getTopicPattern";
-        String pattern = KronosEngineConstants.KafkaTelemetryProcessor.topic(context.getApplicationEngine().getId(),
-                suffix);
-        logInfo(method, "pattern: %s", pattern);
-        return Pattern.compile(pattern);
-    }
+	@Override
+	public void start() {
+		String method = "start";
+		try {
+			String topic = getTopicPattern().pattern();
+			logInfo(method, "checking topic: %s ...", topic);
+			AdminClient client = AdminClient.create(admin.getConfig());
+			Set<String> existingTopics = client.listTopics().names().get();
+			if (existingTopics.contains(topic)) {
+				logInfo(method, "topic already exists: %s", topic);
+			} else {
+				logInfo(method, "creating topic: %s", topic);
+				client.createTopics(Collections.singleton(new NewTopic(topic, 3, (short) 1)));
+			}
+		} catch (Exception e) {
+			logError(method, "error checking topic", e);
+		}
+		super.start();
+	}
 
-    @Override
-    protected void process(String topic, String key, String message) {
-        String method = "KafkaTelemetryProcessorAbstract.process";
-        try {
-            doProcessTelemetry(JsonUtils.fromJson(message, TelemetryWrapper.class));
-        } catch (Exception e) {
-            logError(method, e);
-        }
-    }
+	@Override
+	protected Pattern getTopicPattern() {
+		String method = "getTopicPattern";
+		String pattern = KronosEngineConstants.KafkaTelemetryProcessor.topic(context.getApplicationEngine().getId(),
+				suffix);
+		logInfo(method, "pattern: %s", pattern);
+		return Pattern.compile(pattern);
+	}
 
-    protected KronosEngineContext getContext() {
-        return context;
-    }
+	@Override
+	protected void process(String topic, String key, String message) {
+		String method = "KafkaTelemetryProcessorAbstract.process";
+		try {
+			doProcessTelemetry(JsonUtils.fromJson(message, TelemetryWrapper.class));
+		} catch (Exception e) {
+			logError(method, e);
+		}
+	}
 
-    protected String getEnvProperty(String property, String defaultValue) {
-        return env.getProperty(String.format("%s.%s", getClass().getName(), property), defaultValue).trim();
-    }
+	protected KronosEngineContext getContext() {
+		return context;
+	}
 
-    @Override
-    protected int getNumConsumers() {
-        return Integer.parseInt(getEnvProperty("numThreads", "" + KronosEngineConstants.DEFAULT_PROCESSOR_NUM_THREADS));
-    }
+	protected String getEnvProperty(String property, String defaultValue) {
+		return env.getProperty(String.format("%s.%s", getClass().getName(), property), defaultValue).trim();
+	}
 
-    protected abstract void doProcessTelemetry(TelemetryWrapper wrapper);
+	@Override
+	protected int getNumConsumers() {
+		return Integer.parseInt(getEnvProperty("numThreads", "" + KronosEngineConstants.DEFAULT_PROCESSOR_NUM_THREADS));
+	}
+
+	protected abstract void doProcessTelemetry(TelemetryWrapper wrapper);
 }
